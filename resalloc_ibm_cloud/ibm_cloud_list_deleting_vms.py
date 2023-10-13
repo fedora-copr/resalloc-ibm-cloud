2b9c4ae7c82eaa5aa2847ca8e6e4869722b57950
Pavel Raiskup <praiskup@redhat.com>
2023-10-13 08:37:44 +0200

    Fix vm listing traceback and manpage strings
    Traceback (most recent call last):
      File "/usr/bin/resalloc-ibm-cloud-list-vms", line 8, in <module>
        sys.exit(main())
                 ^^^^^^
      File "/usr/lib/python3.12/site-packages/resalloc_ibm_cloud/ibm_cloud_list_vms.py", line 18, in main
        pool_id = opts.pool or os.getenv("RESALLOC_POOL_ID")
                  ^^^^^^^^^
    AttributeError: 'Namespace' object has no attribute 'pool'
    
    The manpages contained `argparsers.py` string, instead of the utility
    name, therefore defining prog= now.

--- resalloc_ibm_cloud/ibm_cloud_list_deleting_vms.py
+++ resalloc_ibm_cloud/ibm_cloud_list_deleting_vms.py
@@ -3,14 +3,14 @@ List all IBM Cloud instances that are in Deleting state
 """
 
 from resalloc_ibm_cloud.helpers import get_service
-from resalloc_ibm_cloud.argparsers import default_arg_parser
+from resalloc_ibm_cloud.argparsers import list_deleting_vms_parser
 from resalloc_ibm_cloud.constants import LIMIT
 
 
 def main():
     """Entrypoint to the script."""
 
-    opts = default_arg_parser().parse_args()
+    opts = list_deleting_vms_parser().parse_args()
     cmd = f"source {opts.token_file} ; echo $IBMCLOUD_API_KEY"
     service = get_service(cmd, opts)
 
