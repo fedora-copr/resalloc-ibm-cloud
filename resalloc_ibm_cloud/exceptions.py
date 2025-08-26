65d4330c51f66851cb14f34df307eed31ab39540
Jiri Kyjovsky <j1.kyjovsky@gmail.com>
2025-08-26 16:32:16 +0200

    powervs: fix listing of resources

--- resalloc_ibm_cloud/exceptions.py
+++ resalloc_ibm_cloud/exceptions.py
@@ -1,12 +1,13 @@
 class ResallocIBMCloudException(Exception):
     """Base exception class for Resalloc IBM Cloud errors."""
-    pass
 
 
 class PowerVSException(ResallocIBMCloudException):
     """Base exception class for PowerVS errors."""
 
-
 class PowerVSNotFoundException(PowerVSException):
     """Exception raised when a PowerVS resource is not found."""
-    pass
+
+
+class PowerVSInvalidNameException(PowerVSException):
+    """Exception raised when a PowerVS resource name is invalid."""
